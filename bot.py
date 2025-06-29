5a6e076b035465e01de027646212138faecfc1f7
ICHIKAWA Kento <kent@tahoiya.org>
2025-06-30 05:09:15 +0900

    20250630

--- bot.py
+++ bot.py
@@ -4,18 +4,17 @@ import pandas as pd
 from utsuho import HiraganaToKatakanaConverter
 
 class Agent:
-    def __init__(self, game, letters_list, f_name='data/words.csv', lf_name='data/letters.csv'):
+    def __init__(self, game, f_name='data/words.csv'):
         w_bank = pd.read_csv(f_name)
-        l_bank = pd.read_csv(lf_name)
         w_bank = w_bank[w_bank['words'].str.len()==game.letters]
-        w_bank['words'] = w_bank['words'].str.upper() #Convert all words to uppercase
         cnv = HiraganaToKatakanaConverter()
+        w_bank['words'] = w_bank['words'].str.upper() #Convert all words to uppercase
         w_bank['words'] = w_bank['words'].apply(cnv.convert) #Convert all words to カタカナ
+        letters_list =  [chr(code) for code in range(0x30A0, 0x31F0)]
         # word bank for precise prediction
         self.w_bank = w_bank
         # word bank for letter-nallowing prediction
         self.pw_bank = w_bank[w_bank['words'].apply(lambda w: len(set(w)) == len(w))].copy()
-        self.l_bank = l_bank
         self.game = game
         self.prediction = ['' for _ in range(game.letters)]
         self.y_letters = {}
@@ -23,6 +22,7 @@ class Agent:
         self.g_max = 0
         self.used_letters = []
         self.possible_letters = set(letters_list)
+        self.last_letters = set(letters_list)
         pd.set_option('display.max_rows', None)
 
     def calc_letter_probs(self):
@@ -65,7 +65,9 @@ class Agent:
 
     def choose_action(self):
         self.parse_board()
-        nallow_predictions = False
+        nallow_prediction = False
+        manygreen_prediction = False
+        y_len = len(self.y_letters)
         if len(self.g_letters) > 0:
             self.w_bank = self.w_bank[~self.w_bank['words'].str.contains('|'.join(self.g_letters))]
             self.g_letters = []
@@ -78,26 +80,45 @@ class Agent:
             self.y_letters = {}
         if len(self.used_letters) < 2:
             print("Used letters less than 2: nallow mode")
-            nallow_predictions = True
-        if  self.g_max >= 4 :
-            print("4 green letters: prefer nallow mode")
-            nallow_predictions = True
+            nallow_prediction = True
+        # print("predictions: " +str(self.prediction))
+        if self.g_max >= 3 and y_len == 0 and \
+                (self.game.letters - self.g_max) * (self.game.rows - self.game.g_count - 1) \
+                < len(self.w_bank) and \
+                (self.game.rows - self.game.g_count) > 1:
+            print("many green letters: nallow mode")
+            nallow_prediction = True
+            manygreen_prediction = True
+            not_green_letters = set()
+            not_green = set()
+            for i,s in enumerate(self.prediction):
+                if self.prediction[i] == '':
+                    not_green.add(i)
+            for i,r in self.w_bank.iterrows():
+                for j,w in enumerate(r['words']):
+                    for k in not_green:
+                        not_green_letters.add(r['words'][k])
+            
         #Recalculate letter position probability
         self.calc_letter_probs()
 
-        # wide mode prediction 
+        # nallowing possible words bank 
         self.pw_bank['w-score'] = [1] * len(self.pw_bank)
         pattern = "[" + "".join(self.possible_letters) + "]"
         self.pw_bank = self.pw_bank[self.pw_bank['words'].apply(lambda w: all(l in self.possible_letters for l in w))]
         if len(self.pw_bank) == 0:
             print("nallow predictions have exausted")
-            nallow_predictions = False
+            nallow_prediction = False
         else:
             for x in range(self.game.letters):
                 self.pw_bank['w-score'] *= self.pw_bank[f'p-{x}']
+                if manygreen_prediction :
+                    self.pw_bank['ng-count'] = [0] * len(self.pw_bank) 
+                    self.pw_bank['ng-count'] = self.pw_bank['words'].apply(lambda x: sum(1 for letter in x if letter in not_green_letters))
+                    self.pw_bank['w-score'] += self.pw_bank['ng-count']
             mpv_bank = self.pw_bank[self.pw_bank['w-score']==self.pw_bank['w-score'].max()]
 
-        # nallow mode  prediction
+        # precise mode  prediction
         for i, s in enumerate(self.prediction):
             if s != '':
                 self.w_bank = self.w_bank[self.w_bank['words'].str[i]==s]
@@ -105,15 +126,11 @@ class Agent:
         for x in range(self.game.letters):
             if self.prediction[x] == '':
                 self.w_bank['w-score'] *= self.w_bank[f'p-{x}']
-        if nallow_predictions and len(self.w_bank) < 20 :
-            nallow_predictions = False
-        if nallow_predictions and (self.game.rows - self.game.g_count) <= 4:
-            nallow_predictions = False
-        # if True not in [True for s in self.prediction if s in self.vowels]:
-        #     self.w_bank['w-score'] += self.w_bank['v-count'] / self.game.letters
+        if nallow_prediction and len(self.w_bank) < (self.game.rows - self.game.g_count) :
+            nallow_prediction = False
         mv_bank = self.w_bank[self.w_bank['w-score']==self.w_bank['w-score'].max()]
         
-        if nallow_predictions :
+        if nallow_prediction :
             print(str(len(self.w_bank)) + " words left: letter-nallowing mode")
             result = random.choice(mpv_bank['words'].tolist())
         else :
--- main.py
+++ main.py
@@ -12,10 +12,11 @@ ROWS = 10
 LETTERS = 5
 GAMES = 1000
 
+"""
 cnv = HiraganaToKatakanaConverter()
 
 w_bank = pd.read_csv('data/words.csv')
-letters_filename = 'data/letters.csv'
+# letters_filename = 'data/letters.csv'
 
 w_bank = w_bank[w_bank['words'].str.len()==LETTERS]
 # w_bank['words'] = w_bank['words'].str.upper() #Convert all words to Uppercase
@@ -23,33 +24,22 @@ w_bank = w_bank['words'].apply(cnv.convert) # Convert all words to Katakana
 
 letters_set = set()
 letters_freq = {}
-if os.path.exists(letters_filename):
-    with open(letters_filename) as f:
-        lines = f.read()
-        for line in lines.split("\n"):
-            ltr = str(line)
-            letters_set.update(ltr)
-else:
-    with open(letters_filename, 'w') as f:
-        for word in w_bank['words']:
-            letters_set.update(word)
-            for ltr in word:
-              if ltr in letters_freq :
-                  letters_freq[ltr] += 1
-              else:
-                  letters_freq[ltr] = 1
-        f.write('words, freq\n')
-        for letter in letters_set:
-            f.write(str(letter) +', '+ str(letters_freq[letter]) + '\n')
+for word in w_bank['words'] :
+    letters_set.update(word)
+    for ltr in word:
+        if ltr in letters_freq :
+            letters_freq[ltr] += 1
+        else:
+            letters_freq[ltr] = 1
 letters_list = list(letters_set)
+"""
 
 game = Wordle(
     None,
-    letters_list,
     rows=ROWS,
     letters=LETTERS)
 
-bot = Agent(game,letters_list)
+bot = Agent(game)
 for i in range(ROWS):
     suggest = bot.choose_action()
     print(f'SUGGESTED WORD = {suggest}')
